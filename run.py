03b7a4a0800b8183d9dd33ba8049c103b02f18d6
Dima Gerasimov <karlicoss@gmail.com>
2018-10-01 06:28:12 +0100

    generify running function

--- run.py
+++ run.py
@@ -24,7 +24,7 @@ def get_qm_driver():
 
 driver = None
 
-def run_test(test_url: str, script_getter, params: Dict):
+def run_sim(test_url: str, script_getter, params: Dict):
     global driver
     if driver is None:
         driver = get_qm_driver()
@@ -39,16 +39,45 @@ def run_test(test_url: str, script_getter, params: Dict):
     )
     return results.text
 
-def run_simple_reaction():
-    from js_scripts import get_simple_reaction_script
-    TEST_URL = 'http://www.quantified-mind.com/tests/simple_reaction_time/practice'
-
-    from results import results
+def run_test(test_url, script_getter, results, delays, errors):
+    global driver
     def has_result(delay, errors):
         ll = [x for x in results if x['delay'] == delay and x['errors'] == errors]
         return len(ll) > 0
 
-    for delay in [
+    for delay in delays:
+        for errcount in errors:
+            if has_result(delay, errcount):
+                continue
+            while True:
+                try:
+                    res = run_sim(
+                        test_url,
+                        script_getter=script_getter,
+                        params=dict(
+                            delay=delay,
+                            errors=errcount,
+                            variation=10,
+                        )
+                    )
+                    sys.stdout.write(f"{{ 'delay': {delay:4}, 'errors': {errcount:3}, ")
+                    sys.stdout.flush()
+                    sys.stdout.write(f"'res': '{res}' }},\n")
+                    break
+                except Exception as e:
+                    print("# GETTING NEW DRIVER!")
+                    if driver is not None:
+                        driver.quit() # just in case...
+                        driver = None
+
+def run_simple_reaction():
+    from js_scripts import get_simple_reaction_script
+    from results import results_simple_reaction
+    run_test(
+        'http://www.quantified-mind.com/tests/simple_reaction_time/practice',
+        get_simple_reaction_script,
+        results_simple_reaction,
+        delays=[
                        20,  30,  40,  50,  60,  70,  80,  90,
             100, 110, 120, 130, 140, 150, 160, 170, 180, 190,
             200, 210, 220, 230, 240, 250, 260, 270, 280, 290,
@@ -56,66 +85,30 @@ def run_simple_reaction():
             400, 410, 420, 430, 440, 450, 460, 470, 480, 490,
             500, 510, 520, 530, 540, 550, 560, 570, 580, 590,
             600, 610, 620, 630, 640, 650, 660, 670, 680, 690,
-    ]:
-        for errors in [
+        ],
+        errors=[
                 0,
                 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
                 17, 18, 19,
-        ]:
-            # if has_result(delay, errors):
-            #     continue
-            while True:
-                try:
-                    res = run_test(
-                        TEST_URL,
-                        script_getter=get_simple_reaction_script,
-                        params=dict(
-                            delay=delay,
-                            errors=errors,
-                            variation=10,
-                        )
-                    )
-                    sys.stdout.write(f"{{ 'delay': {delay:4}, 'errors': {errors:3}, ")
-                    sys.stdout.flush()
-                    sys.stdout.write(f"'res': '{res}' }},\n")
-                    break
-                except Exception as e:
-                    pass # try again
+        ],
+    )
 
 def run_visual_matching():
-    global driver
     from js_scripts import get_visual_matching_script
-    TEST_URL = "http://www.quantified-mind.com/tests/feature_match/practice"
-    for delay in [
+    from results import results_visual_matching
+    run_test(
+        "http://www.quantified-mind.com/tests/feature_match/practice",
+        get_visual_matching_script,
+        results_visual_matching,
+        delays=[
             # ok, so my delay was 742 on average... so let's do from 20 to 1000 in steps of 20?
             *range(20, 1200, 20),
             *range(30, 1200, 20),
-    ]:
-        for errors in [
-                0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
-        ]:
-            while True:
-                try:
-                    res = run_test(
-                        TEST_URL,
-                        script_getter=get_visual_matching_script,
-                        params=dict(
-                            delay=delay,
-                            errors=errors,
-                            variation=10,
-                        )
-                    )
-                    sys.stdout.write(f"{{ 'delay': {delay:4}, 'errors': {errors:3}, ")
-                    sys.stdout.flush()
-                    sys.stdout.write(f"'res': '{res}' }},\n")
-                    break
-                except Exception as e:
-                    print("GETTING NEW DRIVER!")
-                    if driver is not None:
-                        driver.quit() # just in case...
-                        driver = None
+        ],
+        errors=[
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+        ],
+    )
 
-
-# run_simple_reaction()
-
-run_visual_matching()
+run_simple_reaction()
+# run_visual_matching()
